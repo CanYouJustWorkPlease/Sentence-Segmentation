18e26bc77e408f583afa8bf07b178e1b6d0d38aa
Bartek <yougotmebeggingyouformercy@gmail.com>
2021-04-09 19:42:02 +0200

    functioning multiple spaces in segmentation

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,19 +1,33 @@
 'use strict';
 var elem;
-var doubleSpace;
+var whichSeparator;
 $(document).ready(function() {
-    doubleSpace = false;
+    whichSeparator = 'newLine'; // default
 
-    chrome.runtime.sendMessage({method: "getDoubleSpace"}, function(response) {
-        if (response.message == "On"){
-            console.log("border turned On");
-            doubleSpace = true;
-        }
-        else{
-            doubleSpace = false;
-        }
+    chrome.runtime.sendMessage({method: "getWhichSeparator"}, function(response) {
+        whichSeparator = response.message || 'newLine';
     });
 });
+
+function generateLineBreak() {
+    let lineBreak; 
+    if (whichSeparator === 'newLine') {
+        lineBreak = ".<br/>";
+    }
+    else if (whichSeparator === '2spaces') {
+        lineBreak = ".&nbsp;&nbsp;";
+    }
+    else if (whichSeparator === '3spaces') {
+        lineBreak = ".&nbsp;&nbsp;&nbsp;";
+    }
+    else if (whichSeparator === '4spaces') {
+        lineBreak = ".&nbsp;&nbsp;&nbsp;&nbsp;";
+    }
+    else if (whichSeparator === '5spaces') {
+        lineBreak = ".&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    }
+    return lineBreak;
+}
 
 var segmentSection = function(){
     elem = "body";
@@ -21,9 +35,9 @@ var segmentSection = function(){
         var v = $(this).html();
         var regexForPeriod = /\.\s/g;       //regex to find period
         var regexForQuestion = /\?\s/g;     //regex to find question mark
-        var lineBreak = doubleSpace?".&nbsp;&nbsp;":".<br/>";
-        var questionBreak = "?<br/>";
-        var separatorElem = doubleSpace?"":"<span class='segmentSeparator'></span>";
+        const lineBreak = generateLineBreak();
+        var questionBreak = "?" + lineBreak.slice(1);
+        var separatorElem =  whichSeparator === 'newLine' ? "<span class='segmentSeparator'></span>" : "";
         v = v.replace(regexForPeriod, (lineBreak + separatorElem));        //replacing period with period and newline
         v = v.replace(regexForQuestion, (questionBreak + separatorElem));      //replacing questionmark with questionmark and newline
         $(this).html(v);        //adding replaced content back to the DOM element
