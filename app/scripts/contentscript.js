2416b41d844ae33c8999b5f07f2290ee08daf15c
CanYouJustWorkPlease <73692469+CanYouJustWorkPlease@users.noreply.github.com>
2022-11-02 17:28:51 +0200

    Fixed issue related to html tags angle brackets "<"

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -61,12 +61,13 @@ const segmentSection = function(){
             //
             // Example for "Two new lines" over here:
             // https://regex101.com/r/TJPQH1/2
-            const regexForSymbolsAdvanced = /((?:[\.\?\!][^\.\?\!]*){2})([\.\?\!])[\s\n]/g;
+            const regexForSymbolsAdvanced = /((?:[\.\?\!][^\.\?\!]*){2})([\.\?\!])([\s\n<])/g;
             
             // Replacing every third consecutive period, question mark or exclamation mark (in a random placement)
             // followed by a space or newline character with the character to be replaced and newlines and/or 
             // separation line.
-            v = v.replace(regexForSymbolsAdvanced, ("$1$2" + lineBreak + separatorElem));
+            // v = v.replace(regexForSymbolsAdvanced, ("$1" + '<span style="background-color: red">' + "$2" + '</span>' + lineBreak + separatorElem + "$3"));
+            v = v.replace(regexForSymbolsAdvanced, ("$1" + "$2" + lineBreak + separatorElem + "$3"));
             
             // After enabling any of the two new features in the options, and clicking the icon for the second
             // time, you'll notice the paragraph shrunk. Clicking the icon over and over it will turn the paragraph
@@ -78,11 +79,12 @@ const segmentSection = function(){
             
             // Regex to find every period, question mark or exclamation mark followed
             // by a space character.
-            const regexForSymbolsSimple = /([\.\?\!])\s/g;
+            const regexForSymbolsSimple = /([\.\?\!])([\s\n<])/g;
             
             // Replacing every period, question mark or exclamation mark followed by a space character
             // with the character to be replaced and space(or multiple spaces) or newline.
-            v = v.replace(regexForSymbolsSimple, ("$1" + lineBreak + separatorElem)); 
+            // v = v.replace(regexForSymbolsSimple, ('<span style="background-color: red">' + "$1" + '</span>' + lineBreak + separatorElem + "$2")); 
+            v = v.replace(regexForSymbolsSimple, ("$1" + lineBreak + separatorElem + "$2")); 
             
         }
         // Adding replaced content back to the DOM element.
